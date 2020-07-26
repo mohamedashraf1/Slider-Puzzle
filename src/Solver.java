b940fa9875f317909997d7eca36f9e54d7e14953
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2020-07-26 15:41:09 +0200

    updated the main function to test better

--- src/Solver.java
+++ src/Solver.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
@@ -121,13 +122,26 @@ public class Solver {
 
     // test client (see below)
     public static void main(String[] args) {
-        int[][] arr = {{0, 1, 3}, {4, 2, 5}, {7, 8, 6}};
-        Board initial = new Board(arr);
-        Solver answer = new Solver(initial);
-        StdOut.print(answer.moves());
-        StdOut.print(answer.isSolvable());
-        for (Board board : answer.solution()) {
-            StdOut.print(board.toString());
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
         }
     }
 
